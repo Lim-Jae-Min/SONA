3c552825643bf17a9cda06e5a8ca968798ab24c3
lim_jae_min <woals7297@naver.com>
2024-04-18 17:02:23 +0900

    login 및 회원가입 페이지

--- src/main/java/com/sona/music/member/controller/MemberController.java
+++ src/main/java/com/sona/music/member/controller/MemberController.java
@@ -1,10 +1,18 @@
 package com.sona.music.member.controller;
 
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.sona.music.member.dto.MemberDTO;
 import com.sona.music.member.service.MemberService;
 
 @Controller
@@ -14,4 +22,52 @@ public class MemberController {
 	
 	@Autowired MemberService memberService;
 	
+	@RequestMapping(value="/joinform.go")
+	public String joinform(Model model) {
+		logger.info("회원가입 페이지 접근");
+		
+		return "member/joinform";
+	}
+	
+	
+	
+	
+	@RequestMapping(value="/login.do")
+	public String login(String id, String pw, Model model, HttpSession session) {
+		String page = "member/login";
+		logger.info("id : {} | pw : {}", id, pw);
+		
+		
+		MemberDTO info = memberService.login(id, pw);
+		logger.info("loginId : "+ info.getUser_id().toString());
+		
+		if(info != null) {
+			page = "redirect:/main/main";
+			session.setAttribute("logininfo", info);	
+		}else {
+			model.addAttribute("msg","아이디 또는 비밀번호 확인해주세요");
+		}
+				
+		return page;
+	
+	}
+	/*
+	@RequestMapping(value="/join.do")
+	public String join(Model model, @RequestParam Map<String, String> param) {
+		logger.info("회원가입 요청");
+		String msg = "회원가입에 실패하였습니다.";
+		String page = "joinForm";
+		
+		int row = memberService.join(param);
+		logger.info("회원가입 row 반환값: "+ row);
+		
+		if(row == 1) {
+			page = "login";
+			msg = "회원가입에 성공하였습니다.";
+		}
+		
+		model.addAttribute("msg",msg);		
+		return page;
+	}
+*/	
 }
